fd0fdaa8cbab3709e712c0c70acb62d7d9fc5b5a
AnyaKarpiuk <anna-19.1994@mail.ru>
2020-08-30 15:56:59 +0000

    test

--- src/server.js
+++ src/server.js
@@ -3,12 +3,15 @@ const path = require('path');
 
 const app = express();
 
+const http = require('http');
+const server = http.Server(app);
+
 app.use('/static', express.static('public'));
 
 app.get ('/', function(req, res){
     res.sendFile(path.join(__dirname, '/main.html'));
 });
 
-app.listen(process.env.port || 8080);
+server.listen(process.env.port || 8080);
 
 
