196d18fa5fc7b73f5a09ef3a057720303dbe8fb5
AnyaKarpiuk <anna-19.1994@mail.ru>
2020-08-30 15:44:14 +0000

    test

--- src/server.js
+++ src/server.js
@@ -11,10 +11,4 @@ app.get ('/', function(req, res){
 
 app.listen(process.env.port || 8080);
 
-var static = require('node-static');
-var file = new static.Server();
-require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    file.serve(request, response);
-  }).resume();
-}).listen(process.env.PORT || 3000);
+
